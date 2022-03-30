ac1bc9f63ae0a01e954ae916625e9eccb89dfc92
585011 <585011@stud.hvl.no>
2022-03-30 21:01:12 +0200

    Ferdig

--- Assign3/About.aspx.cs
+++ Assign3/About.aspx.cs
@@ -41,15 +41,13 @@ namespace Assign3
                    
                     GridView1.DataSource = quer1.ToList();
                     GridView1.DataBind();
-                    //GridView1_PageIndexChanging();
 
                     GridView1.Visible = true;
                     GridView2.Visible = false;
                     GridView3.Visible = false;
 
                 }
-                //Berre for å sjekke ka value'n va
-                //TextBox1.Text = DropDownList1.SelectedItem.Value;
+
                 if (DropDownList2.SelectedItem.Value != "0")
                 {
                     char c = char.Parse(DropDownList2.SelectedItem.Text);
@@ -57,7 +55,6 @@ namespace Assign3
                     var query2 = dx.student.Select(stud => new { stud.studentname, stud.id })
                         .Join(dx.grade, stud => stud.id, gr => gr.studentid, (stud, gr) => new { stud.studentname, gr.coursecode,gr.grade1 })
                         .Join(dx.course, gr => gr.coursecode, course => course.coursecode, (gr, course) => new { gr.studentname, gr.grade1, course.coursename })
-                        //.Where(g => g.grade1.CompareTo(DropDownList2.SelectedItem.Text) >= 0);
                         .Where(g => String.Compare(g.grade1, DropDownList2.SelectedItem.Text) <= 0);
 
                     GridView2.DataSource = query2.ToList();
@@ -69,15 +66,6 @@ namespace Assign3
                     GridView1.Visible = false;
 
                 }
-                if (TextBox1.Text.Length == 0 && (DropDownList1.SelectedItem.Value == "0") && (DropDownList2.SelectedItem.Value == "0"))
-                {
-                    Button1.Click += new EventHandler(this.Button1_Click);
-                    GridView3.Visible = false;
-                    GridView2.Visible = true;
-                    GridView1.Visible = false;
-                }
-                //EventHandler EventHandler = new EventHandler(GridView1_SelectedIndexChanged);
-                //Button2.Click += new EventHandler(this.Button2_Click);
             }
 
             else
@@ -104,10 +92,7 @@ namespace Assign3
             DropDownList2.Items.Add(new ListItem("D"));
             DropDownList2.Items.Add(new ListItem("E"));
             DropDownList2.Items.Add(new ListItem("F"));
-            //foreach (grade g in gr)
-            //{
-            //    DropDownList2.Items.Add(new ListItem(g.grade1, g.coursecode));
-            //}
+
         }
 
         protected void Button1_Click(object sender, EventArgs e)
@@ -115,17 +100,18 @@ namespace Assign3
             var query2 = dx.student.Select(stud => new { stud.studentname, stud.id })
                         .Join(dx.grade, stud => stud.id, gr => gr.studentid, (stud, gr) => new { stud.studentname, gr.coursecode, gr.grade1 })
                         .Join(dx.course, gr => gr.coursecode, course => course.coursecode, (gr, course) => new { gr.studentname, gr.grade1, course.coursename })
-                        //.Where(g => g.grade1.CompareTo(DropDownList2.SelectedItem.Text) >= 0);
                         .Where(g => String.Compare(g.grade1, "F") == 0);
 
             GridView2.DataSource = query2.ToList();
             GridView2.DataBind();
+
+
+            GridView3.Visible = false;
+            GridView2.Visible = true;
+            GridView1.Visible = false;
         }
 
-        //protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
-        //{
-        //    GridView2.PageIndex = e.NewPageIndex;
-        //}
+
 
         protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
         {
@@ -141,22 +127,17 @@ namespace Assign3
                 
                 var queG1 = dx.grade.FirstOrDefault(g => g.studentid == que.id);
                 var queG = dx.grade.FirstOrDefault(g => g.student.studentname == que.studentname);
-                //var query2 = dx.grade.Select(stud => new { stud.studentid, stud.grade1 })
-                //        .Join(dx.student, stud => stud.studentid, gr => gr.id, (stud, gr) => new { stud.grade1, gr.id, gr.studentname})
-                //        .Where(s => s.id == que.id).FirstOrDefault().ToString();
-                //var queC = dx.course.FirstOrDefault(c => c.);
+
                 if (que != null)
                 {
-                    //dx.grade.Remove(query2);
+
                     dx.grade.Remove(queG);
-                    //dx.course.Remove(queC);
-                    //dx.student.Remove(que);
+
                     dx.SaveChanges();
                     GridView1.DataBind();
-                    //Response.Redirect(Request.Url.AbsoluteUri);
+                    Response.Redirect(Request.Url.AbsoluteUri);
                     
                 }
-                //GridView1.DataBind();
             }
         }
 
@@ -175,14 +156,15 @@ namespace Assign3
             };
             student s = new student
             {
-                studentage = 69,
+                studentage = 20,
                 studentname = "Ny",
                 id = Convert.ToInt32(TextBox2.Text)
             };
-            //dx.student.Add(s);
+            dx.student.Add(s);
             dx.grade.Add(g);
             dx.SaveChanges();
-            
+            Response.Redirect(Request.Url.AbsoluteUri);
+
 
         }
     }
